3c985fb7a0ac032b682c779f5d6f1598d70e1694
Student's Account <student@labkom.local>
2024-02-12 16:25:04 +0800

    ASP INFORMATIKA

---
+++ pengurangan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //kalau bisa gak usa di otak atik lagi
+    cout << "Berhasil OIIII" << endl;
+
+    int a, b, hasil;
+    cin >> a;
+    cin >> b;
+    // hasil = a - b;
+    // hasil = a * b;
+    hasil = a % b;
+    cout << hasil << endl;
+    return 0;
+}
